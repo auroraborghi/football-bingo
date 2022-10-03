7ddadeec934ec7cecdf4b0282ccd2dafeafec2d5
Aurora Borghi <auroraborghi@Auroras-MacBook-Pro.local>
2022-10-02 21:40:16 -0400

    Sorted Questions based on observations, chat with partner, and reading WAYYYY too many NFL reddit posts. o.o

--- football-bingo-ui/src/question-bank.tsx
+++ football-bingo-ui/src/question-bank.tsx
@@ -4,13 +4,35 @@ export const COMMON_QUESTIONS = [
     'Field Goal',
     'Passing Touchdown',
     'Rushing Touchdown',
-    'Fumble!'
+    'Fumble!',
+    '4th Down Defensive Stop',
+    '4th Down Conversion',
+    'QB Sneak',
+    'Sack',
+    'Tipped Pass',
+    'Sportscaster mentions Retired Player by Name',
+    'Interception',
+    'Reverse Play',
+    'Coach Challenges Play',
+    'Punt inside 10 yard line',
+    'Screen pass going for 1st Down',
 ] as string[];
 
 export const UNCOMMON_QUESTIONS = [
-    'Player Helmet Comes Off'
+    'Player\'s Helmet Comes Off',
+    '50 yard Passing Play',
+    '100 Yard Rushing Game',
+    'Missed Field Goal',
+    '2 Point Conversion',
+    'Offensive Play that loses 7 or more yards',
+    'Team erases 10+ point deficit by end of game',
+    'One Handed Catch',
+    'More than 5 penalties in a game for one team'
 ] as string[];
 
 export const RARE_QUESTIONS = [
-    'Successful Stiff Arm'
+    'Successful Stiff Arm',
+    'Overtime',
+    'Blocked punt/kick',
+    'Defensive Safety'
 ] as string[];
